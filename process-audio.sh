da05808ae2bba83a32bb146550c6cbf839e1e7d8
Richard Lubomski <Bomski@Richards-iMac.local>
2014-09-19 16:07:03 +0100

    minor update to bash file

--- process-audio.sh
+++ process-audio.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
 # converts all .mp3 files in the subfolders of where
-# this script is executed to
-# contain 1 second of silence at the start and end of the file
+# this script is executed to contain 1 second of silence 
+# at the start and end of the file.
 #
 # all converted files will be in the new subdirectory
 # "converted" inside the directory where this script is
-# executed; the original files will not be touched
+# executed; the original files will not be touched.
 #
 # you have to install sox prior to running this script
 # http://sox.sourceforge.net/
@@ -28,12 +28,15 @@ for dir in *;
      cd $dir
      
 	read -p "WARNING: this will modify all .mp3 files contained within directories in \"$PWD\", Do you wish to proceed? (y/n)" CONT
+	
 	if [ "$CONT" == "y" ]; then
     
     echo "adding silence to start and end of .mp3 files in \"$PWD\"." 
+     
      if [ ! -d ../converted/$dir ]; then
        mkdir ../converted/$dir
      fi
+     
      for i in *.mp3; do
        echo "Converting Input File: \"./$dir/$i\"..."
        sox "$i" "../converted/$dir/$i" pad 1 1
